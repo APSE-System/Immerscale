e4d43d96050e751d05405f56e5894e048fb3fa70
Marcel <marcelflashinc@gmail.com>
2024-03-19 09:21:50 +0100

    Set up vue Project and play around with values

---
+++ WorkerView/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://immersight.com/" target="_blank"
+      >Immersight</a
+    >, the official sponsor of this project
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
