a7b756be71608da58fedd4cf1625a6f867511298
Marcel <marcelflashinc@gmail.com>
2024-03-19 11:44:27 +0100

    Add prototype for WorkerView page wiht working projects list using dummy values

--- WorkerView/src/components/HelloWorld.vue
+++ WorkerView/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import Button from 'primevue/button';
 
 defineProps({
   msg: String,
@@ -12,8 +13,9 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
   </div>
+  <Button label="Success" severity="success" />  
+
 
   <p>
     Check out
---
+++ WorkerView/src/components/ProjectComponent.vue
@@ -0,0 +1,61 @@
+<script setup>
+import 'primeicons/primeicons.css'
+
+//define properties for the project items
+
+const PROPS = defineProps({
+  id: Number,
+  name: String,
+  active: Boolean,
+})
+
+//call this when the user clicks a project to view its details
+function clickedProject() {
+  console.log('Project ' + PROPS.id + ' clicked')
+  alert('Project ' + PROPS.id + ' clicked')
+  //insert backend call here
+}
+</script>
+
+
+<template>
+  <!-- Empty div to work around weird visibility issue -->
+  <div></div>
+  <div class="ProjectItem" @click="clickedProject">
+    <!-- Icon on the left side -->
+    <div class="pi pi-user" style="font-size: 8rem"></div>
+    <!-- Values taken from properties -->
+    <div class="ItemInfos">
+      <p>{{ name }}</p>
+      <p>Project id: {{ id }}</p>
+      <p>Activity status: {{ active }}</p>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.ProjectItem {
+  background-color: #797979;
+  padding: 0.5rem;
+  border: 1px solid #e1e1e1;
+  border-radius: 7px;
+  transition: transform 0.3s ease;
+  width: 100%;
+  max-width: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 15px;
+}
+
+.ProjectItem:hover {
+  transform: scale(1.04);
+}
+
+.ItemInfos {
+  display: block;
+  gap: 0rem;
+}
+</style>
