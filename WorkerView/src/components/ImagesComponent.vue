b934967f5bdfb6203b071fe856355c2adddf1ad8
Johannes <mj0st.jo@gmail.com>
2024-03-20 08:31:12 +0100

    add basic image page routing

---
+++ WorkerView/src/components/ImagesComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div>
+    <!-- The current route is accessible as $route in the template -->
+    Project {{ $route.params.id }}
+  </div>
+</template>
+
+
+<style scoped>
+</style>
--- WorkerView/src/router/index.js
+++ WorkerView/src/router/index.js
@@ -1,9 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 import App from '../App.vue'
+import Images from '../components/ImagesComponent.vue'
 
+// Define the routes for the application here
 const routes = [
   { path: '/', component: App },
+  { path: '/project/:id', component: Images },
 ]
 
 const router = createRouter({
