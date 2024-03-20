e8167ec358deb007646b40f4173c5494feb36c7a
Johannes <mj0st.jo@gmail.com>
2024-03-20 10:47:09 +0100

    Add image display logic

--- WorkerView/src/components/ProjectPage.vue
+++ WorkerView/src/components/ProjectPage.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { onMounted } from 'vue'
 
+var Images = []
+
 // When the component is mounted the images which refer to this component should be fetched from the backend.
 onMounted(() => {
   fetch('http://' + import.meta.env.VITE_BACKEND_IP + '/api/image')
     .then(function() {
-      console.log(`Received Images`)
+      // set the images array according to the backend response
     })
     .catch(function(){
       alert("Backend ist nicht errreichbar")
@@ -16,11 +18,25 @@ onMounted(() => {
 
 <template>
   <div>
-    <!-- This will be updated as soon as the backend endpoints are implemented -->
-    Project {{ $route.params.id }}
+    <h2>Projekt {{ $route.params.id }}</h2>
+    <!-- Depending wether there were images fetched from the backend they are displayed or not. -->
+    <div v-if="Images.length">
+      <!-- Iterating through the image list and displaying them. -->
+      <div v-for="(image, index) in Images" :key="index">
+        <!-- Add the logic here for clicking on an image and opening the measuring editor. -->
+        <img :src="image" alt="Image {{ index + 1 }}">
+      </div>
+    </div>
+    <div v-if="!Images.length">
+      <h3>In diesem Projekt sind noch keine Bilder.</h3>
+    </div>
   </div>
 </template>
 
 
 <style scoped>
-</style>../env.js../../env.js
+  img {
+    width: 400px; 
+    object-fit: cover;
+  }
+</style>
