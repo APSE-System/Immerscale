a3c002ec6d4546c1d6ad1e0f53b33c2c3505ba16
Johannes <mj0st.jo@gmail.com>
2024-03-21 16:06:38 +0100

    FIx cors headers

--- WorkerView/src/components/ProjectPage.vue
+++ WorkerView/src/components/ProjectPage.vue
@@ -1,13 +1,19 @@
 <script setup>
 import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
 
 var Images = []
 
 // When the component is mounted the images which refer to this component should be fetched from the backend.
 onMounted(() => {
-  fetch('http://' + import.meta.env.VITE_BACKEND_IP + '/api/image')
-    .then(function() {
-      // set the images array according to the backend response
+  fetch('http://' + import.meta.env.VITE_BACKEND_IP + '/workerView/images?id=' + route.params.id)
+    .then((resp) => resp.json())
+    .then(function(data) {
+      for (var key in data){
+        Images.push("data:image/png;base64," + data[key].image)
+      }
     })
     .catch(function(){
       alert("Backend ist nicht errreichbar")
@@ -24,7 +30,8 @@ onMounted(() => {
       <!-- Iterating through the image list and displaying them. -->
       <div v-for="(image, index) in Images" :key="index">
         <!-- Add the logic here for clicking on an image and opening the measuring editor. -->
-        <img :src="image" alt="Image {{ index + 1 }}">
+        <!--<img :src="image" alt="Image {{ index + 1 }}">-->
+        <h1>{{ image }}</h1>
       </div>
     </div>
     <div v-if="!Images.length">
