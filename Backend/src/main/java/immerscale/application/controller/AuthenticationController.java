f7e0e3ac12e813ac43d86236774515dc270b8bbe
Daniel Stempfle <stempfledaniel@gmx.de>
2024-03-25 14:59:21 +0100

    Debug authentication controller

--- Backend/src/main/java/immerscale/application/controller/AuthenticationController.java
+++ Backend/src/main/java/immerscale/application/controller/AuthenticationController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,15 +17,16 @@ import java.util.Optional;
 
 // The controller is used for authentication
 // Unauthorized Requests wont be filtered
-@RestController(value = "/authentication")
+@RestController
+@RequestMapping( value = "/auth")
 public class AuthenticationController {
 
-    @Autowired
-    AccessTokenRepository accessTokenRepository;
 
     // Method issues a cookie to the enduser, when a valid token is passed
     // This cookie allows access to all endpoints from the PhotoViewController
-    @GetMapping("/photoView")
+    @Autowired
+    AccessTokenRepository accessTokenRepository;
+    @GetMapping("/cookie/enduser")
     public ResponseEntity<String> verifyEnduser(@RequestParam(value = "tokend_id") String tokend_id, HttpServletResponse response){
 
         Optional<AccessToken> accessToken = accessTokenRepository.findById(tokend_id);
@@ -32,7 +34,7 @@ public class AuthenticationController {
         // Token if present in database -> issue cookie
         if(accessToken.isPresent()){
             response.addCookie(new Cookie("BenisUserCookie", tokend_id));
-            ResponseEntity.ok("Success");
+            return ResponseEntity.ok("Success");
         }
 
         return new ResponseEntity<String>("Unauthorized",HttpStatus.UNAUTHORIZED);
@@ -42,12 +44,12 @@ public class AuthenticationController {
     // Method issues a cookie to the worker, when a valid token is passed
     // This cookie allows access to all endpoint from the WorkerViewController
     // TODO: Verification Logic must be added, currently every caller gets a valid cookie
-    @GetMapping("/photoView")
-    public ResponseEntity<String> verifyWorker(@RequestParam(value = "tokend_id") String tokend_id, HttpServletResponse response){
+    @GetMapping("/cookie/worker")
+    public ResponseEntity<String> verifyWorker(@RequestParam(value = "token_id") String token_id, HttpServletResponse response){
 
          if(true){
             response.addCookie(new Cookie("BenisWorkerCookie", "123456789"));
-            ResponseEntity.ok("Success");
+            return ResponseEntity.ok("Success");
         }
 
         return new ResponseEntity<String>("Unauthorized",HttpStatus.UNAUTHORIZED);
