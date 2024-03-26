504dd9dfd5a1a12b5dc7bf1054c146ab87c2de64
Johannes <mj0st.jo@gmail.com>
2024-03-26 15:18:30 +0100

    highlight only selected button

--- WorkerView/src/components/TabBar.vue
+++ WorkerView/src/components/TabBar.vue
@@ -1,18 +1,39 @@
 <script setup>
+    import { onMounted, ref } from "vue";
     import Button from 'primevue/button';
-    import { useRouter } from 'vue-router'
+    import { useRouter, useRoute } from 'vue-router'
 
     // get the router for routing to the project sub pages
     const router = useRouter()
+    const route = useRoute()
+
+    // this value is used to determine which button should be highlighted. This depends on which tab is selected.
+    const primary = ref(0); 
+
+    // As the tabs can be accessed directly via the URL, the path has to be checked which tab is selected to highlight the according button.
+    onMounted(()=>{
+        const pathSegments = route.path.split('/')
+        switch (pathSegments[pathSegments.length - 1]) {
+            case 'images': 
+                primary.value = 1;
+                break;
+            case 'access': 
+                primary.value = 2;
+                break;
+            case 'settings': 
+                primary.value = 3;
+                break;
+        }
+    })
 
 </script>
 
 
 <template>
     <div class="sidebar card flex justify-content-left">
-        <Button label="Bilder" @click="router.push('images')"/>
-        <Button label="Zugriffe" @click="router.push('access')"/>
-        <Button label="Projekteinstellungen" @click="router.push('settings')"/>
+        <Button label="Bilder" @click="router.push('images'); primary = 1;" :class="['p-button-' + ((primary === 1) ? 'primary' : 'secondary')]"/>
+        <Button label="Zugriffe" @click="router.push('access'); primary = 2;" :class="['p-button-' + ((primary === 2) ? 'primary' : 'secondary')]"/>
+        <Button label="Projekteinstellungen" @click="router.push('settings'); primary = 3;" :class="['p-button-' + ((primary === 3) ? 'primary' : 'secondary')]"/>
     </div>
 </template>
 
