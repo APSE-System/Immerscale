3be226058f9192b5dcc1661b6383ce0391cf6148
Daniel Stempfle <stempfledaniel@gmx.de>
2024-04-04 11:08:14 +0200

    Add model class

---
+++ WorkerView/src/components/Editor/Logic/Model.js
@@ -0,0 +1,50 @@
+class CanvasPoint {
+    x;
+    y;
+    color;
+
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+}
+
+class CanvasLine {
+    startPoint;
+    endPoint;
+    color;
+
+    constructor(startPoint, endPoint, color) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+        this.color = color;
+    }
+}
+
+class Model {
+    canvasPoints = [];
+    canvasLines = [];
+
+    addPoint(x, y, color) {
+        this.canvasPoints.push(new CanvasPoint(x, y, color));
+    }
+
+    removePoint(x, y, color) {
+        let index = this.canvasPoints.findIndex(point => point.x === x && point.y === y && point.color === color);
+        if (index !== -1) {
+            this.canvasPoints.splice(index, 1);
+        }
+    }
+
+    addLine(startX, startY, endX, endY, color) {
+        this.canvasLines.push(new CanvasLine(new CanvasPoint(startX, startY), new CanvasPoint(endX, endY), color));
+    }
+
+    removeLine(startX, startY, endX, endY, color) {
+        let index = this.canvasLines.findIndex(line => line.startPoint.x === startX && line.startPoint.y === startY && line.endPoint.x === endX && line.endPoint.y === endY && line.color === color);
+        if (index !== -1) {
+            this.canvasLines.splice(index, 1);
+        }
+    }
+}
