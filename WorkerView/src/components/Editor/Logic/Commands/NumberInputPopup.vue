fa3c969fe01b4549dd1bea602e62f4137a5691a9
Johannes <mj0st.jo@gmail.com>
2024-04-10 16:56:28 +0200

    Only make popup closable on valid values

--- WorkerView/src/components/Editor/Logic/Commands/NumberInputPopup.vue
+++ WorkerView/src/components/Editor/Logic/Commands/NumberInputPopup.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps} from "vue";
+import {defineProps} from "vue";
 import InputNumber from 'primevue/inputnumber';
 import Dialog from "primevue/dialog";
 import Button from "primevue/button";
@@ -11,6 +11,15 @@ const props = defineProps({
 })
 
 
+function saveButtonClicked(value) {
+  console.log(value);
+  if (value !== undefined && value !== null && value > 0) {
+    props.popup.visible = false;
+    emit('callback', value);
+  }
+}
+
+
 </script>
 
 <template>
@@ -20,10 +29,11 @@ const props = defineProps({
       <span class="p-text-secondary block mb-3">{{ popup.description }}</span>
       <div class="flex align-items-center gap-3 mb-3">
         <label for="name" class="font-semibold w-6rem">{{ popup.value }}</label>
-        <InputNumber id="name" class="flex-auto" autocomplete="off" locale="de-DE" mode="decimal" :minFractionDigits="2" :useGrouping="false" v-model="numberValue"/>
+        <InputNumber id="name" class="flex-auto" autocomplete="off" locale="de-DE" mode="decimal" :minFractionDigits="2"
+                     :useGrouping="false" v-model="numberValue"/>
       </div>
       <div class="flex justify-content-end gap-2">
-        <Button type="button" label="Save" @click="popup.visible = false; emit('callback', numberValue); numberValue=null;"></Button>
+        <Button type="button" label="Save" @click="saveButtonClicked(numberValue); numberValue=null;"></Button>
       </div>
     </Dialog>
   </div>
