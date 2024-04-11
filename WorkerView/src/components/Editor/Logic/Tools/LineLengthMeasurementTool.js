3a4a0721aa00a8f84f1a0b5fefeada8baf71fc46
Marcel <marcelflashinc@gmail.com>
2024-04-11 15:18:44 +0200

    Add V1 of measuring capability

--- WorkerView/src/components/Editor/Logic/Tools/LineLengthMeasurementTool.js
+++ WorkerView/src/components/Editor/Logic/Tools/LineLengthMeasurementTool.js
@@ -8,6 +8,13 @@ import AddLineCommand from "../Commands/AddLineCommand.js";
 class LineLengthMeasurementTool extends MeasurementTool {
     // The first command executed by this tool.
     _first = null;
+
+    // The coordinates of the first and second point selected by the user.
+    _firstX = 0;
+    _firstY = 0;
+    _secondX = 0;
+    _secondY = 0;
+
     // The amout of points that the user has already selected in the image.
     _pointCount = 0;
     
@@ -23,14 +30,20 @@ class LineLengthMeasurementTool extends MeasurementTool {
         // Checking how many points were already specified by the user and adding new commands accordingly.
         if (this._pointCount == 0) {
             // The first command is simply adding a point to the user selected coordinates.
-            this._model.do(new AddPointCommand(this, this._model, x, y));
+            this._firstX = x;
+            this._firstY = y;
+            this._model.do(new AddPointCommand(this, this._model, this._firstX, this._firstY));
             console.log("Point 1 added");
         } else if (this._pointCount == 1) {
             // The second step draws a line from the first point to the newly selected point.
-            this._model.do(new AddPointCommand(this, this._model, x, y));
+            this._secondX = x;
+            this._secondY = y;
+            this._model.do(new AddPointCommand(this, this._model, this._secondX, this._secondY));
             // Draw the line between the two points
-            this._model.do(new AddLineCommand(this, this._model, [[this._first.getX(), this._first.getY()], [x, y]], false));
+            this._model.do(new AddLineCommand(this, this._model, [[this._first.getX(), this._first.getY()], [this._secondX, this._secondY]], false));
             // Set the tool to finished
+            let result = this.measureLength();
+            console.log(result);
             this._finished = true;
             // Reset the point count and the first point to null for the next use of the tool
             this._first = null;
@@ -64,6 +77,14 @@ class LineLengthMeasurementTool extends MeasurementTool {
         while (this._model.undo() != this._first) {
             // Undoes all the commands done by this tool if it is not yet finished
         }
+    }
+
+    measureLength() {
+        let point_1 = LordImmerScaler.transformToRealWorld(this._firstX, this._firstY);
+        console.log(point_1);
+        let point_2 = LordImmerScaler.transformToRealWorld(this._secondX, this._secondY);
+        console.log(point_2);
+        return MathUtils.getDistance(point_1[0], point_1[1], point_2[0], point_2[1]);
     }
 
 }
