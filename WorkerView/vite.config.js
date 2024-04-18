f75908e3b9f75bfabfbe054b29f2881deb9dd046
Johannes <mj0st.jo@gmail.com>
2024-04-18 15:21:28 +0200

    update docker stuff

--- WorkerView/vite.config.js
+++ WorkerView/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    https: true,
     watch: {
       usePolling: true,
       interval: 100, // Check files every 100ms
     },
   },
-  
-  plugins: [vue()],
+  plugins: [vue(), basicSsl()],
 })
