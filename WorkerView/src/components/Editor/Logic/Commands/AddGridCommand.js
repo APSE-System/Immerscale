4ff2c0b5d94716cf169a63fcd3d7ebe2aada18d6
Johannes <mj0st.jo@gmail.com>
2024-04-23 12:16:56 +0200

    finish method extraction

--- WorkerView/src/components/Editor/Logic/Commands/AddGridCommand.js
+++ WorkerView/src/components/Editor/Logic/Commands/AddGridCommand.js
@@ -36,12 +36,17 @@ class AddGridCommand extends Command{
         this._gridWidth = 10;
         this._gridHeight = 10;
 
+        // Generate the grid
         this.generateGridPoints()
-        let points_rotated = this.rotateGridPoints()
-        this.positionCamera(points_rotated)
-        this.projectGridPoints(points_rotated)
-        this.scaleGridPoints()
-        this.offsetPoints()
+
+        // Retrieve the right projection fitting to the specified angles
+        let points_rotated = this.rotatePoints(this._points)
+        let points_positioned = this.positionCamera(points_rotated)
+        this.projectOnGrid(points_positioned)
+
+        // Position the grid with the fitting scale in the  middle of the canvas
+        this.scaleGrid()
+        this.offsetGrid()
 
     }
 
@@ -57,93 +62,55 @@ class AddGridCommand extends Command{
         }
     }
 
-    rotateGridPoints(){
+    rotatePoints(points){
         let points_rotated = []
 
-        for(let i = 0; i < this._points.length; i++){
-            let point = this._points[i];
+        for(let i = 0; i < points.length; i++){
+            let point = points[i];
 
             let result = rotatePoints3Dim3Axes( [point.x, point.y, 0, 1], this._xRot, this._yRot, this._zRot);
             let result_formatted = [result[0]._data[0], result[0]._data[1], result[0]._data[2], result[0]._data[3]]
 
-
             points_rotated.push(result_formatted)
-
         }
 
         return points_rotated;
     }
 
-    positionCamera(points_rotated){
-        //constole.log("View projection")
+    positionCamera(points){
+        let points_positioned = []
         for(let i = 0; i < this._points.length; i++){
-            let point = points_rotated[i]
+            let point = points[i]
 
             let result = viewMatrix( point);
             let result_formatted = [result[0]._data[0], result[0]._data[1], result[0]._data[2], result[0]._data[3]]
 
-            //constole.log("result" + JSON.stringify(result_formatted))
-
-            points_rotated[i] = result_formatted
-
+            points_positioned.push(result_formatted)
         }
+
+        return points_positioned;
     }
 
-    projectGridPoints(points_rotated){
-
-        //constole.log("After VIEWMATRIX")
-        //constole.log(points_rotated)
-        // projection erection
-
-        //constole.log("PROJECTION OF POINTS")
+    projectOnGrid(points){
         for(let i = 0; i < this._points.length; i++){
-            let point = points_rotated[i]
+            let point = points[i]
 
+            let result = projectPointTo2D(point)
 
-            let result = projectPointTo2D(point )
-            //constole.log(result);
-
-            let brightness = ( result[0]._data[2]/ result[0]._data[3])-8
-
-            brightness *= 50
-
-            brightness -= 44
-            brightness *= 10
-
-            if(brightness < 9)
-                brightness = 10
-
-            if(brightness > 99)
-                brightness = 99
-
-
-
-            //constole.log(brightness + ", brightness");
-
-
-            this._points[i] = new CanvasPoint( result[0]._data[0]/ result[0]._data[3],  result[0]._data[1]/result[0]._data[3], "" +brightness + brightness + brightness);
+            this._points[i] = new CanvasPoint( result[0]._data[0]/ result[0]._data[3],  result[0]._data[1]/result[0]._data[3], this._points[i].color);
         }
     }
 
-    scaleGridPoints(){
-        //constole.log("AFTER PROJECTION OF POINTS")
-        //constole.log(this._points)
-
-        // scaleus to biggus
+    scaleGrid(){
         for(let i = 0; i < this._points.length; i++){
             let point = this._points[i];
             let result = scalePoint2D([point.x, point.y], 200)
 
-
-
             this._points[i]  = new CanvasPoint(result[0]._data[0], result[0]._data[1], point.color)
-
         }
     }
 
-    offsetPoints(){
-        //benis offset
-        //constole.log("OFFSET")
+    offsetGrid(){
         let xOffset = (this._model.width/2) + this._xPos
         let yOffset = (this._model.height/2) + this._yPos
         for(let i = 0; i < this._points.length; i++){
@@ -154,7 +121,6 @@ class AddGridCommand extends Command{
 
 
     execute (){
-
         this._model.addGrid(new CanvasGrid(this._points, this._gridWidth, this._gridHeight));
     }
 
